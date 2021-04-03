9b8190bdee2e6fbef81c05a55837c2227ef56128
Arangojd26 <arangojd26@gmail.com>
2021-04-03 18:03:46 -0500

    interfaz del inicio mejorada con sus animaciones

--- cheese-rescue-app/src/app/pages/HomePage/Home.page.jsx
+++ cheese-rescue-app/src/app/pages/HomePage/Home.page.jsx
@@ -5,11 +5,11 @@ import "./Home.page.scss";
 const Home = () => {
   return (
     <div>
-      <div className="o-layer-background" />
-      <div className="container-fluid">
-        <div className="o-container-start">
+      {/* <div className="o-layer-background" /> */}
+      <div className="o-container-start">
+        <div className="o-content-start">
           <ButtonStart />
-          <div className="o-text-jugar">Jugar</div>
+          <div className="o-text-jugar">Play</div>
         </div>
       </div>
     </div>
